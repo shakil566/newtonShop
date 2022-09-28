1827e33ca47df11781d2faa19a53c8904a668496
shakil566 <shakilhossen566@gmail.com>
2022-09-28 22:44:40 +0600

    login for user and admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,8 +27,15 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    public function authenticated()
+    {
+        if (Auth::user()->role_as == '1') {
+            return redirect('/dashboard')->with('status', 'Welcome Admin dashboard');
+        } else if (Auth::user()->role_as == '0') {
+            return redirect('/')->with('status', 'Welcome dear user');
+        }
+    }
     /**
      * Create a new controller instance.
      *
